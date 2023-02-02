bf60eaca733d16995d858feac850fa12991a37d8
Enzo Deshayes <enzodeshayes@MacBook-Pro-de-Enzo.local>
2023-02-02 23:08:53 +0100

    first commit

---
+++ src/components/user/Login.js
@@ -0,0 +1,53 @@
+import React, {Component, useState} from 'react';
+import Dashboard from "../dashboad/dashboard";
+
+const Login = () => {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const baseUrl = "https://aled-weather.fr:8080/token"
+
+    const returnPage = () => {
+        Dashboard()
+    }
+
+    const handleSend = () => {
+        fetch(baseUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userEmail: email,
+                userPassword: password
+            })
+        })
+            .then(async (response) => {
+                if (response.status === 200) {
+                    let responseJson = await response.json();
+                    console.log(responseJson);
+                    setIsLoggedIn(true);
+                    returnPage()
+                } else {
+                    console.log('Erreur de connexion');
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    return (
+        <>
+            <input placeholder="userEmail" onChangeText={text => setEmail(text)}/>
+            <inputt placeholder="userPassword" onChangeText={text => setPassword(text)}/>
+            <button title="Envoyer" onPress={handleSend}/>
+        </>
+    );
+
+}
+
+export default Login;
